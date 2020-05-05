371bc55647a8d5f59979f9d4927674ee1038922e
Lineu Pastorelli <54123248+lineuxyz@users.noreply.github.com>
2020-05-05 10:47:04 -0300

    add the sign in page

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -1,9 +1,12 @@
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useCallback} from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
+import { Link, useHistory } from 'react-router-dom'
 
+import { useAuth } from '../../hooks/auth';
+import { useToast } from '../../hooks/toast'
 import getValidationsErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
@@ -11,12 +14,23 @@ import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Content, Background } from './styles';
+import { Container, Content, AnimationContainer, Background } from './styles';
+
+interface SignInFormData {
+  email: string;
+  password: string;
+}
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const handleSubmit = useCallback(async (data: object) => {
+  const { signIn } = useAuth();
+
+  const { addToast } = useToast();
+
+  const history = useHistory();
+
+  const handleSubmit = useCallback(async (data: SignInFormData) => {
     try {
       formRef.current?.setErrors({});
 
@@ -30,16 +44,34 @@ const SignIn: React.FC = () => {
       await schema.validate(data, {
         abortEarly: false,
       });
-    } catch (err) {
-      const errors = getValidationsErrors(err);
 
-      formRef.current?.setErrors(errors);
+      await signIn({
+        email: data.email,
+        password: data.password,
+      })
+      
+      history.push('/dashboard');
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationsErrors(err);
+
+        formRef.current?.setErrors(errors);
+
+        return
+      }
+ 
+      addToast({
+        type: 'error',
+        title: 'Error na autenticação',
+        description: 'Ocorreu um erro ao fazer login, cheque as credenciais.'
+      });
     }
-  }, []);
+  }, [signIn, addToast, history]);
 
   return (
     <Container>
       <Content>
+        <AnimationContainer>
         <img src={logoImg} alt="GoBarber" />
 
         <Form ref={formRef} onSubmit={handleSubmit}>
@@ -59,10 +91,11 @@ const SignIn: React.FC = () => {
           <a href="forgot">Esqueci minha senha</a>
         </Form>
 
-        <a href="Login">
+        <Link to="./signup">
           <FiLogIn />
           Criar conta
-        </a>
+        </Link>
+        </AnimationContainer>
       </Content>
 
       <Background />
