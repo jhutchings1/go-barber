5883df8ba5f7c172f008046d51f92e4594f8de9a
Lineu Pastorelli <lineualberto@gmail.com>
2020-05-14 20:22:48 -0300

    uppload configurations to uppload the assets avatar

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -2,18 +2,19 @@ import path from 'path';
 import crypto from 'crypto';
 import multer from 'multer';
 
-const tmpFolder = path.resolve(__dirname, '..', '..', 'temp')
+const tmpFolder = path.resolve(__dirname, '..', '..', 'temp');
 
 export default {
-    directory: tmpFolder,
+  tmpFolder,
+  uploadsFolder: path.resolve(tmpFolder, 'uploads'),
 
-    storage: multer.diskStorage({
-        destination: tmpFolder,
-        filename(request, file, callback) {
-            const fileHash = crypto.randomBytes(10).toString('HEX');
-            const fileName = `${fileHash}-${file.originalname}`;
+  storage: multer.diskStorage({
+    destination: tmpFolder,
+    filename(request, file, callback) {
+      const fileHash = crypto.randomBytes(10).toString('HEX');
+      const fileName = `${fileHash}-${file.originalname}`;
 
-            return callback(null, fileName);
-        },
-    }),
+      return callback(null, fileName);
+    },
+  }),
 };
