8c45a406ca8494c2415c962e213e53f715b47a50
Lineu Pastorelli <lineualberto@gmail.com>
2020-05-23 12:34:09 -0300

    add global variations

--- backend/src/config/auth.ts
+++ backend/src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
-    jwt: {
-        secret: 'df1e411c74a5641620ece2d40ec454ce',
-        expiresIn: '1d',
-    }
-}
+  jwt: {
+    secret: process.env.APP_SECRET,
+    expiresIn: '1d',
+  },
+};
