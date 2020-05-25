2693da2e31ed80f4cdf152e990b37339b55301a2
Lineu Pastorelli <lineualberto@gmail.com>
2020-05-25 11:53:15 -0300

    add new architecture to routes profile, users and resed/forgot password

---
+++ backend/src/modules/users/infra/http/routes/users.routes.ts
@@ -0,0 +1,35 @@
+import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
+
+import multer from 'multer';
+import uploadConfig from '@config/upload';
+
+import UserAvatarController from '../Controllers/UserAvatarController';
+import UsersController from '../Controllers/UsersController';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
+const usersRouter = Router();
+const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
+const upload = multer(uploadConfig);
+
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create,
+);
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  userAvatarController.update,
+);
+
+export default usersRouter;
